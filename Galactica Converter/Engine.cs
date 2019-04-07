9b6b184f0974348f8fe797908282ecf0339f00c8
EmpekPL <michal.bdg@gmail.com>
2019-04-07 21:58:40 +0200

    Initial Commit

---
+++ Galactica Converter/Engine.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galactica_Converter
+{
+    public static class Engine
+    {
+        public static decimal calculate(int from, int to, decimal value, int switchIndicator)
+        {
+            if (switchIndicator == 0) // 0: convert from space to earth units 
+                                      // 1: from earth to space units  
+            {
+                switch (from)
+                {
+                    case 0: //Light-second
+
+                        switch (to)
+                        {
+
+                            case 0: // metre
+                                return value * (decimal)299792543.55986;
+                            case 1: // kilometre
+                                return value * (decimal)299792.54355986;
+                            case 2: // Nautical mile
+                                return value * (decimal)161875.08244295;
+                            default:
+                                return (decimal)-1.00;
+
+                        }
+
+                    case 1: //Light-minute
+
+                        switch (to)
+                        {
+
+                            case 0: // metre
+                                return value * (decimal)17987552613.591;
+                            case 1: // kilometre
+                                return value * (decimal)17987552.613591;
+                            case 2: // Nautical mile
+                                return value * (decimal)9712504.946577;
+                            default:
+                                return (decimal)-1.00;
+
+                        }
+                    case 2: //Astronomical unit
+
+                        switch (to)
+                        {
+
+                            case 0: // metre
+                                return value * (decimal)149597806297.77;
+                            case 1: // kilometre
+                                return value * (decimal)149597806.29777;
+                            case 2: // Nautical mile
+                                return value * (decimal)80776382.695124;
+                            default:
+                                return (decimal)-1.00;
+                        }
+
+                    case 3: // Light-year
+
+                        switch (to)
+                        {
+
+                            case 0: // metre
+                                return value * (decimal)9.4605589298216E+15;
+                            case 1: // kilometre
+                                return value * (decimal)9460558929821.6;
+                            case 2: // mila morska
+                                return value * (decimal)161875.08244295;
+                            default:
+                                return (decimal)-1.00;
+
+                        }
+                    case 4: // parsec
+
+                        switch (to)
+                        {
+
+                            case 0: // metre
+                                return value * (decimal)30856769049426 * 1000;
+                            case 1: // kilometre
+                                return value * (decimal)30856769049426;
+                            case 2: // Nautical mile
+                                return value * (decimal)16661328445621;
+                            default:
+                                return (decimal)-1.00;
+
+                        }
+
+
+                }
+            }
+            else // 1: convert from earth to space units
+            {
+                switch (from)
+                {
+                    case 0: //metre
+                        switch (to)
+                        {
+                            case 0: //Light-second
+                                return value / (decimal)299792543.55986;
+                            case 1: // Light-minute
+                                return value / (decimal)17987552613.591;
+                            case 2: // Astronomical unit
+                                return value / (decimal)149597806297.77;
+                            case 3: // Light-year
+                                return value / (decimal)9.4605589298216E+15;
+                            case 4: //parsec
+                                return value / (30856769049426 * 1000);
+                            default:
+                                return (decimal)-1.00;
+                        }
+                    case 1:
+                        switch (to)
+                        { // kilomentre
+                            case 0: // Light-second
+                                return value / (decimal)299792.54355986;
+                            case 1: // Light-minute
+                                return value / (decimal)17987552.613591;
+                            case 2: // Astronomical unit
+                                return value / (decimal)149597806.29777;
+                            case 3: // Light-year
+                                return value / (decimal)9460558929821.6;
+                            case 4: //parsec
+                                return value / 30856769049426;
+                            default:
+                                return (decimal)-1.00;
+                        }
+                    case 2: // Nautical mile
+                        switch (to)
+                        {
+                            case 0: // Light-second
+                                return value / (decimal)9712504.946577;
+                            case 1: // Light-minute
+                                return value / (decimal)9712504.946577;
+                            case 2: // Astronomical unit
+                                return value / (decimal)80776382.695124;
+                            case 3: // Light-year
+                                return value / (decimal)161875.08244295;
+                            case 4: // parsec
+                                return value / 16661328445621;
+                            default:
+                                return (decimal)-1.00;
+                        }
+                    default:
+                        return (decimal)-1.00;
+                }
+                
+            }
+            return (decimal)-1.00;
+
+
+
+
+        }
+           
+
+
+        }
+
+    }
+
---
+++ Galactica Converter/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Galactica_Converter
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int swichInicator = 0; // 0: spaca to earth units   1: earth to space units
+        public MainWindow()
+        {
+            // WELCOME WINDOW 
+            WelcomeWindow welcome = new WelcomeWindow();
+            welcome.Show();
+            
+            System.Threading.Thread.Sleep(3000); // WAIT 3 SEK
+            welcome.Close();
+
+
+            InitializeComponent();
+            createUnits(0);
+            
+
+        }
+
+        private void createUnits(int x)
+        {
+            //calcutating units
+            string[] unitsSpace = {  "light-second", "light-minute",
+                "astronimical unit", "light-year", "parsec"};
+
+            string[] unitsEarth = { "metre [m]", "kilometre [km]", "nautical mile [nm]" };
+
+            //clear comboBox is useful for switching between units group
+            fromComboBox.Items.Clear();
+            toComboBox.Items.Clear();
+
+            if (x == 0) { 
+            //creat comboBox Items
+            foreach(string unit in unitsSpace)
+            {
+                fromComboBox.Items.Add(unit);
+            }
+
+            foreach (string unit in unitsEarth)
+            {
+                toComboBox.Items.Add(unit);
+            }
+            }
+            else
+            {
+                foreach (string unit in unitsSpace)
+                {
+                    toComboBox.Items.Add(unit);
+                }
+
+                foreach (string unit in unitsEarth)
+                {
+                    fromComboBox.Items.Add(unit);
+                }
+            }
+        }
+
+        //converting units
+        private void calculateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int x;
+            int y;
+            String value = "0";
+           
+
+            value = enterValueBox.Text;
+            x = fromComboBox.SelectedIndex;
+            y = toComboBox.SelectedIndex;
+            decimal z = 0;
+            try { 
+            z = decimal.Parse(value);
+            }
+            catch (FormatException)
+            {
+                resultBlock.Text = "wrong entered value";
+                return;
+            }
+            //validation 
+            if (x >= 0 && y >= 0 && z>0) { 
+                decimal result = Engine.calculate(x, y, z, swichInicator);
+                String resultFormated;
+                try
+                { //Fromating result : 12 345 678 901
+                    if (result > 1)
+                    {
+                        resultFormated = String.Format("{0:#,#}", result);
+                    }
+                    else
+                    {   //it`s not working well
+                        resultFormated = String.Format("{#.###,###,###,###,###,###,###}", result);
+                    }
+                }
+                catch (FormatException)
+                {
+                    resultFormated = result.ToString();
+                }
+              
+            resultBlock.Text = resultFormated;
+
+            }
+            else { resultBlock.Text = "entered value is under 0 or didn`t choose units"; }
+                       
+        } 
+
+      // SWITCHING BETWEEN UNITS ( SPACE TO EARTH AND EARTH TO SPACE)
+        private void switchUnits(object sender, RoutedEventArgs e)
+        {
+            
+
+            if (swichInicator==0)
+            {
+                swichInicator++;
+                createUnits(swichInicator);
+            }
+            else
+            {
+                swichInicator--;
+                createUnits(swichInicator);
+            }
+        }
+    }
+}
